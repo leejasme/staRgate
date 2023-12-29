ccf7a503a92e53dd004e9ce0a67297a5df28a88d
LeeJ22 <lee.jasme@gmail.com>
2023-12-29 16:04:02 -0500

    update vignette and test/add example for density gating with multiple markers supplied
    updated the function to internally handle the lapply() instead of making user specify the lapply() and passing 1 marker at a time
    - compiled rmd and it works!

--- R/get_density_gates.R
+++ R/get_density_gates.R
@@ -77,7 +77,7 @@ get_density_gates = function(intens_dat,
     rlang::abort(message = "Error: `intens_dat` must be of data.frame class")
   }
 
-  if(!(marker %in% colnames(intens_dat))){
+  if(!all(marker %in% colnames(intens_dat))){
     rlang::abort(message = "Error: `marker` must be string matching column name(s) of `intens_dat`")
   }
 
@@ -117,7 +117,7 @@ get_density_gates = function(intens_dat,
   if(!is.null(neg_intensity_threshold)){
     i_dat =
       intens_dat %>%
-      dplyr::filter(!(if_any(all_of(markers_to_gate), ~.x < neg_intensity_thres)))
+      dplyr::filter(!(if_any(all_of(markers_to_gate), ~.x < neg_intensity_threshold)))
 
   }else{
     i_dat = intens_dat
@@ -126,65 +126,78 @@ get_density_gates = function(intens_dat,
   # 2023-12-28 check pos_peak_threshold is structured correctly if it is not a numeric
   if(!(inherits(pos_peak_threshold, "numeric"))){
     # If not a single numeric, then check if its a df of correct format, otherwise warn
-    if(!(iinherits(pos_peak_threshold, "data.frame"))){
+    if(!(inherits(pos_peak_threshold, "data.frame"))){
       rlang::warn(message = c( "Warning: `pos_peak_threshold` must be a numeric or dataframe",
                                "i" = "Default value `pos_peak_threshold = 1800` will be used."))
 
-      pos_peak_threshold = 1800
+      pos_peak_threshold =
+        tibble::tibble(
+          MARKER = marker,
+          POS_PEAK_THRESHOLD = 1800
+        )
+
     }else{
       # If dataframe, then check if structured correctly
       # two columns with names marker and pos_peak_threshold
       # Change the colnames of pos_peak_thresholds to all caps if df
-      pos_peak_thresholds =
-        pos_peak_thresholds %>%
+      pos_peak_threshold =
+        pos_peak_threshold %>%
         janitor::clean_names(case = "all_caps")
 
-      chk1 = all(colnames(pos_peak_thresholds) %in% c("MARKER", "POS_PEAK_THRESHOLD"))
+      chk1 = all(colnames(pos_peak_threshold) %in% c("MARKER", "POS_PEAK_THRESHOLD"))
 
       # Also check if all the names in marker column match those in the `marker` arg
-      chk2 = all( marker %in% pos_peak_thresholds$marker)
+      chk2 = all(marker %in% pos_peak_threshold$MARKER)
 
-      if(!(chk1 & chk2)){
-        rlang::abort(message = c("Error: `pos_peak_thresholds` is not of correct format",
-                                 "i" = "Column names for `pos_peak_thresholds` should be `marker` and `pos_peak_threshold`",
-                                 "i" = "Not all markers in `marker` argument have a `pos_peak_threshold` supplied."))
-      }else if(!chk1){
-        rlang::abort(message = c("Error: `pos_peak_thresholds` is not of correct format",
-                                 "i" = "Column names for `pos_peak_thresholds` should be `marker` and `pos_peak_threshold`"))
-      }else if(!chk2){
-        rlang::warn(message = c("Error: `pos_peak_thresholds` is not of correct format",
-                                "i" = "Not all markers in `marker` argument have a `pos_peak_threshold` supplied.",
+      # If the col names of pos_peak_threshold are correct but not all markers have a corresponding threshold
+      if(!chk2 & chk1){
+        rlang::warn(message = c("Error: Not all markers in `marker` argument have a `pos_peak_threshold` supplied",
                                 "i" = "Default value of `pos_peak_threshold = 1800` will be used for the markers without a value supplied."))
 
         # which names are not in the `marker` arg?
-        nms_to_fill = marker[!(marker %in% pos_peak_thresholds$marker)]
+        nms_to_fill = marker[!(marker %in% pos_peak_threshold$MARKER)]
 
         # dplyr::add_row should add a row per string in the nms_to_fill vector with pos_peak_threshold fixed
-        pos_peak_thresholds =
-          pos_peak_thresholds %>%
+        pos_peak_threshold =
+          pos_peak_threshold %>%
           dplyr::add_row(
             MARKER = nms_to_fill,
             POS_PEAK_THRESHOLD = 1800
           )
+      }else if(!(chk1 & chk2)){
+        rlang::abort(message = c("Error: `pos_peak_threshold` is not of correct format",
+                                 "i" = "Column names for `pos_peak_threshold` should be `marker` and `pos_peak_threshold`",
+                                 "i" = "Not all markers in `marker` argument have a `pos_peak_threshold` supplied."))
+      }else if(!chk1){
+        rlang::abort(message = c("Error: `pos_peak_threshold` is not of correct format",
+                                 "i" = "Column names for `pos_peak_threshold` should be `marker` and `pos_peak_threshold`"))
       }
     }
+  }else{
+    pos_peak_threshold =
+      tibble::tibble(
+        MARKER = marker,
+        POS_PEAK_THRESHOLD = pos_peak_threshold
+      )
   }
 
 
-  # TODO: Wrap around the `marker` supplied
+  # Wrap around the `marker` supplied
   # make them lists
   dens_binned =
     lapply(marker,
            function(m){
+
              # Grab the pos peak threshold corresponding to the current marker to gate
              p_threshold =
                pos_peak_threshold %>%
                dplyr::filter(MARKER == m) %>%
                dplyr::pull(POS_PEAK_THRESHOLD)
 
+
             # Still expects just 1 marker at a time and the pos peak threshold to be a numeric
              get_density_mats(i_dat,
-                              m,
+                              marker = m,
                               subset_col,
                               bin_n = bin_n,
                               peak_detect_ratio = peak_detect_ratio,
@@ -216,7 +229,7 @@ get_density_gates = function(intens_dat,
                                           }) %>%
                                unlist()) %>%
                # dplyr::filter(cd4_pos_cd8_pos != "cd4_neg_cd8_neg") %>%
-               dplyr::select(dplyr::all_of(c(subset_col, marker))) #%>%
+               dplyr::select(dplyr::all_of(c(subset_col, m))) #%>%
              # rename(subpop = cd4_pos_cd8_pos)
            }
     ) %>%
@@ -227,4 +240,5 @@ get_density_gates = function(intens_dat,
                   by = subset_col)
 
   return(cutoffs)
+
 }
